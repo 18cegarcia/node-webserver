e0bf9e49aa54bfe550a5440386c82b0e1e94ab37
18Cegarcia <18cegarcia@gmail.com>
2019-03-26 18:11:54 -0600

    test

--- server/config/config.js
+++ server/config/config.js
@@ -3,9 +3,9 @@
 process.env.PORT=process.env.PORT || 8080;
 process.env.NODE_ENV=process.env_NODE_ENV || 'dev';
 let urldb ;
-if (process.env.NODE_ENV==='dev'){
-  urldb='mongodb://localhost:27017/cafe';
-}else{
+//if (process.env.NODE_ENV==='dev'){
+  //urldb='mongodb://localhost:27017/cafe';
+//}else{
     urldb='mongodb+srv://cegarcia:zF0c4HYkxDv1zfv6@cluster0-zshzn.mongodb.net/cafe';
-}
+//}
 process.env.URLDB=urldb;
