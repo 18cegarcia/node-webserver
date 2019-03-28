36d0b7dc0c27ff6f495675b089c4e643ce2df154
18Cegarcia <18cegarcia@gmail.com>
2019-03-27 20:03:46 -0600

    dd

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ process.env.SED=process.env.SED||'secret-demo' ;
 let urldb;
 //if (process.env.NODE_ENV === 'dev') {
   //  urldb = 'mongodb://localhost:27017/cafe';
-}// else {
+//}// else {
     urldb = process.env.MONG_URI;
 //}
 process.env.URLDB = urldb;
