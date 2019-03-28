41febdb777fb85f81827a00c39bf199127489e63
18Cegarcia <18cegarcia@gmail.com>
2019-03-27 20:01:17 -0600

    everion

--- server/config/config.js
+++ server/config/config.js
@@ -12,11 +12,11 @@ process.env.SED=process.env.SED||'secret-demo' ;
 
 
 let urldb;
-if (process.env.NODE_ENV === 'dev') {
-    urldb = 'mongodb://localhost:27017/cafe';
-} else {
+//if (process.env.NODE_ENV === 'dev') {
+  //  urldb = 'mongodb://localhost:27017/cafe';
+}// else {
     urldb = process.env.MONG_URI;
-}
+//}
 process.env.URLDB = urldb;
 
 
